dc50a35d847a50bd1ab69ca09d11d2c704ca8256
kimgreeny <112922183+kimgreeny@users.noreply.github.com>
2022-10-04 00:04:55 +0900

    Delete vans directory

--- vans/js/vans.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('nav ul li:nth-of-type(1)').mouseenter(function(){
-    $('this').children('div.submenu').stop().slideDown();
-  });
-  $('nav ul li:nth-of-type(1)').mouseleave(function(){
-    $('this').children('div.submenu').stop().slideUp();
-  });
-})
